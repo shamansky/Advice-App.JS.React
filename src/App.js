6b6c298a5c152ff76cdd78e5fb56a94128f9a598
George Ciurescu <shubst3r@gmail.com>
2019-05-16 05:18:36 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Title from './components/Title';
+import Form from './components/Form';
+import Advice from './components/Advice';
 
-function App() {
+
+class App extends React.Component {
+  state = {
+    advice: []
+  }
+  getAdvice = async (e) => {
+    e.preventDefault();
+
+    const api_call = await fetch(`https://api.adviceslip.com/advice`);
+    const data = await api_call.json();
+    // console.log(data.slip.advice);
+    this.setState({
+      advice: data.slip.advice
+    })
+  }
+
+
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="main">
+        <div className="container">
+
+        <Title />
+        <Form getAdvice={this.getAdvice}/>
+        <Advice  advice={this.state.advice}/>
+
+        </div>
+      </div>
     </div>
-  );
+  )
+}
 }
 
 export default App;
