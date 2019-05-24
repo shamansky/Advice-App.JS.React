e3998c5abc1a6c3e216fe155602dde46eb00bd1e
George Ciurescu <shubst3r@gmail.com>
2019-05-24 11:45:19 +0100

    test

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = () => (
   <div>
-  <h2 className="title-container__title"> Want some unrequested advice?</h2>
+  <h2 className="title-container__title"> Want some unrequested advice?!</h2>
   <h5 className="title-container__subtitle">(Of course you do!)</h5>
   </div>
 )
