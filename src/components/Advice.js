b130595c326b8967b0dbee04d8b4c0ce751be4f6
George Ciurescu <shubst3r@gmail.com>
2019-05-24 12:21:13 +0100

    test

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Advice = props => (
     <div className="advice__info">
     <h1 className="advice__key">
-    <span>
+
     {props.advice}
-    </span>
+
     </h1>
     </div>
   )
